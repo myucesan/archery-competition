89c37ec9751c00860856605b925c19c241b3453c
Costa <costa.van.elsas@hva.nl>
2020-10-03 12:53:08 +0200

    Fixed the list to show high to low

--- src/main/java/nl/hva/ict/se/sands/ScoreComparator.java
+++ src/main/java/nl/hva/ict/se/sands/ScoreComparator.java
@@ -4,17 +4,23 @@ import java.util.Comparator;
 
 public class ScoreComparator implements Comparator<Archer> {
 
+    /**
+     * Compare the two archers to see who is higher on the list
+     * @param a first archer
+     * @param b second archer
+     * @return 0
+     */
     @Override
     public int compare(Archer a, Archer b)
     {
-        if (a.getTotalScore() > b.getTotalScore() ) return +1;
-        if (a.getTotalScore() < b.getTotalScore() ) return -1;
-        if (a.getTens() > b.getTens() ) return +1;
-        if (a.getTens() < b.getTens() ) return -1;
-        if (a.getNines() > b.getNines()) return +1;
-        if (a.getNines() < b.getNines()) return -1;
-        if (b.getId() < a.getId()) return +1;
-        if (b.getId() > a.getId()) return -1;
+        if (a.getTotalScore() < b.getTotalScore() ) return +1;
+        if (a.getTotalScore() > b.getTotalScore() ) return -1;
+        if (a.getTens() < b.getTens() ) return +1;
+        if (a.getTens() > b.getTens() ) return -1;
+        if (a.getNines() < b.getNines()) return +1;
+        if (a.getNines() > b.getNines()) return -1;
+        if (b.getId() > a.getId()) return +1;
+        if (b.getId() < a.getId()) return -1;
         return 0;
     }
 }
